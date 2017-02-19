5ea8a5106cc5e077e4f1692d4e6d52e2ac2b91b0
Stanislav Vitko <svitko@griddynamics.com>
2017-02-19 13:47:27 -0800

    add config support

--- telegrapher.go
+++ telegrapher.go
@@ -1,22 +1,57 @@
 package main
 
 import (
-	"github.com/vistrcm/telegrams"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/vistrcm/telegrams"
+	"io/ioutil"
 	"os"
+	"os/user"
 )
 
+const (
+	relativeConfigFilePath = "/.config/telegrapher.json"
+	noConfigToken          = "tokenNotSet"
+	noConfigChat           = "chatNotSet"
+)
+
+type Configuration struct {
+	Token  string `json:"token"`
+	ChatId string `json:"chat"`
+}
+
+func ReadConfig(relativeConfigFilePath string) (config Configuration, err error) {
+	usr, err := user.Current()
+	if err != nil {
+		config = Configuration{Token: noConfigToken, ChatId: noConfigChat}
+		return
+
+	}
+	configFileName := usr.HomeDir + relativeConfigFilePath
+	raw, err := ioutil.ReadFile(configFileName)
+	if err != nil {
+		config = Configuration{Token: noConfigToken, ChatId: noConfigChat}
+		return
+	}
+
+	config = Configuration{}
+	err = json.Unmarshal(raw, &config)
+	if err != nil {
+		config = Configuration{Token: noConfigToken, ChatId: noConfigChat}
+		return
+	}
+
+	return config, nil
+
+}
+
 func main() {
-	const (
-		defaultToken = "defaulttoken"
-		defaultChatID = "111111111111"
-	)
 
+	config, _ := ReadConfig(relativeConfigFilePath)
 
-	var token = flag.String("token", defaultToken, "bot token")
-	var chat = flag.String("chat", defaultChatID, "chat ID to send message to")
-
+	var token = flag.String("token", config.Token, "bot token")
+	var chat = flag.String("chat", config.ChatId, "chat ID to send message to")
 	flag.Parse()
 
 	if flag.NArg() < 1 {
@@ -29,8 +64,8 @@ func main() {
 	bot := telegrams.NewTelegramBot(*token)
 	_, err := bot.SendMessage(*chat, message)
 
-	if err != nil{
-		fmt.Println(err)
-		panic("something goes wrong!" )
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err: %v. token: %v. chat: %v\n", err, *token, *chat)
+		os.Exit(2)
 	}
 }
